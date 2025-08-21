7027d8def68c3189df495be9cec84fb76eb04991
cabetoDLP <cabetodp@gmail.com>
2025-08-20 22:34:01 -0500

    Correccion de tailwind config 4

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [require('daisyui')],
-  daisyui: { themes: ["light", "dark"] },
-};
+  daisyui: {
+    themes: ['light', 'dark'],
+  },
+}
